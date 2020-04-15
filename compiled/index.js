02f6a20f13395e1ef2342b94af529236df687be0
Marissa Conner <marissafromtexas@gmail.com>
2020-04-14 19:39:17 -0700

    initial setup of react and compiler

---
+++ compiled/index.js
@@ -0,0 +1,13 @@
+import ReactDOM from 'react-dom';
+import React from 'react';
+
+const App = () => {
+  return React.createElement(
+    'div',
+    null,
+    'Disregard harlots acquire wealth'
+  );
+};
+
+ReactDOM.render(React.createElement(App, null), document.getElementById('app'));
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIi4uL2luZGV4LmpzeCJdLCJuYW1lcyI6WyJSZWFjdERPTSIsIlJlYWN0IiwiQXBwIiwicmVuZGVyIiwiZG9jdW1lbnQiLCJnZXRFbGVtZW50QnlJZCJdLCJtYXBwaW5ncyI6IkFBQUEsT0FBT0EsUUFBUCxNQUFxQixXQUFyQjtBQUNBLE9BQU9DLEtBQVAsTUFBa0IsT0FBbEI7O0FBRUEsTUFBTUMsTUFBTSxNQUFNO0FBQ2hCLFNBQU87QUFBQTtBQUFBO0FBQUE7QUFBQSxHQUFQO0FBQ0QsQ0FGRDs7QUFJQUYsU0FBU0csTUFBVCxDQUFnQixvQkFBQyxHQUFELE9BQWhCLEVBQXdCQyxTQUFTQyxjQUFULENBQXdCLEtBQXhCLENBQXhCIiwiZmlsZSI6ImluZGV4LmpzIiwic291cmNlc0NvbnRlbnQiOlsiaW1wb3J0IFJlYWN0RE9NIGZyb20gJ3JlYWN0LWRvbSc7XG5pbXBvcnQgUmVhY3QgZnJvbSAncmVhY3QnO1xuXG5jb25zdCBBcHAgPSAoKSA9PiB7XG4gIHJldHVybiA8ZGl2PkRpc3JlZ2FyZCBoYXJsb3RzIGFjcXVpcmUgd2VhbHRoPC9kaXY+XG59XG5cblJlYWN0RE9NLnJlbmRlcig8QXBwLz4sIGRvY3VtZW50LmdldEVsZW1lbnRCeUlkKCdhcHAnKSk7Il19
---
+++ index.jsx
@@ -0,0 +1,8 @@
+import ReactDOM from 'react-dom';
+import React from 'react';
+
+const App = () => {
+  return <div>Disregard harlots acquire wealth</div>
+}
+
+ReactDOM.render(<App/>, document.getElementById('app'));
